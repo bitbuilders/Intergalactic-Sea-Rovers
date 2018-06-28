e381a56824f4878b20644836b25b56fb5d3f01df
Colin Misbach <colin@misbach.com>
2018-06-28 11:53:31 -0600

    Started on weapons | Separated weapons and items in inventory

--- Assets/Scripts/Items/Inventory.cs
+++ Assets/Scripts/Items/Inventory.cs
@@ -5,28 +5,81 @@ using UnityEngine;
 public class Inventory : MonoBehaviour
 {
     [SerializeField] [Range(0, 100)] int m_capacity = 1;
-    [SerializeField] public List<ItemEntity> Items = null;
+    [SerializeField] GameObject m_weapons = null;
+    [SerializeField] GameObject m_items = null;
 
+    public List<Weapon> Weapons { get; private set; }
+    public List<ItemEntity> Items { get; private set; }
     public Entity Owner { get; set; }
     public int Capacity { get { return m_capacity; } set { m_capacity = value; } }
 
-    public bool ObtainItem(Weapon weapon)
+    private void Start()
+    {
+        Weapons = new List<Weapon>();
+        Items = new List<ItemEntity>();
+        UpdateInventory();
+    }
+
+    public bool ObtainItem(ItemEntity item)
     {
         if (Full())
             return false;
 
-        Items.Add(weapon);
+        item.transform.parent = m_items.transform;
+        Items.Add(item);
 
         return true;
     }
 
-    public void DropItem(Weapon weapon)
+    public void DropItem(ItemEntity item)
     {
-        Items.Remove(weapon);
+        Items.Remove(item);
+    }
+
+    public bool ObtainWeapon(Weapon weapon)
+    {
+        if (Full())
+            return false;
+
+        weapon.transform.parent = m_weapons.transform;
+        Weapons.Add(weapon);
+
+        return true;
+    }
+
+    public void DropWeapon(Weapon weapon)
+    {
+        Weapons.Remove(weapon);
     }
 
     private bool Full()
     {
-        return Items.Count >= Capacity;
+        return Items.Count + Weapons.Count >= Capacity;
+    }
+
+    private void UpdateInventory()
+    {
+        UpdateWeapons();
+        UpdateItems();
+    }
+
+    private void UpdateWeapons()
+    {
+        UpdateList(m_weapons, Weapons);
+    }
+
+    private void UpdateItems()
+    {
+        UpdateList(m_items, Items);
+    }
+
+    private void UpdateList<T>(GameObject parent, List<T> list)
+    {
+        list.Clear();
+        T[] ts = m_weapons.GetComponentsInChildren<T>();
+        foreach (T t in ts)
+        {
+            list.Add(t);
+        }
     }
 }
