e69fbc5ee2f53fcc0455f4612d1067d80e0bb352
Colin Misbach <colin@misbach.com>
2018-07-02 12:30:00 -0600

    Working on enemy | Got enemy into the game, working on logic

---
+++ Assets/Scripts/Combat/WeaponCollider.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponCollider : MonoBehaviour
+{
+    [System.Serializable]
+    public struct WeaponInfo
+    {
+        public Collider collider;
+        public string name;
+    }
+
+    [SerializeField] List<WeaponInfo> m_weapons = null;
+
+    public void SetColliderActive(string name)
+    {
+        GetColliderFromName(name).enabled = true;
+    }
+
+    public void SetColliderInactive(string name)
+    {
+        GetColliderFromName(name).enabled = false;
+    }
+
+    private Collider GetColliderFromName(string name)
+    {
+        Collider c = null;
+
+        foreach (WeaponInfo wi in m_weapons)
+        {
+            if (wi.name == name)
+            {
+                c = wi.collider;
+                break;
+            }
+        }
+        
+        return c;
+    }
+}
---
+++ Assets/Scripts/Player/Enemy.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] [Range(0.0f, 10.0f)] float m_jumpResistance = 3.0f;
+    [SerializeField] [Range(0.0f, 10.0f)] float m_fallSpeed = 3.0f;
+    [SerializeField] Transform m_groundTouch;
+    [SerializeField] LayerMask m_groundMask = 0;
+
+    public bool OnGround { get; protected set; }
+
+    EnemyAI m_AI;
+    EnemyPlayer m_player;
+    Animator m_animator;
+    Rigidbody m_rigidbody;
+
+    public bool IsHuman
+    {
+        get
+        {
+            return m_player.enabled;
+        }
+        set
+        {
+            m_player.enabled = value;
+            m_AI.enabled = !value;
+        }
+    }
+
+    private void Start()
+    {
+        m_AI = GetComponent<EnemyAI>();
+        m_player = GetComponent<EnemyPlayer>();
+        m_rigidbody = GetComponent<Rigidbody>();
+        m_animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        Collider[] b = Physics.OverlapSphere(m_groundTouch.position, 0.1f, m_groundMask);
+        OnGround = b.Length > 0;
+        m_animator.SetBool("OnGround", OnGround);
+    }
+
+    private void FixedUpdate()
+    {
+        if (m_rigidbody.velocity.y > 0.01f)
+        {
+            m_rigidbody.velocity += (Vector3.up * Physics.gravity.y) * (m_jumpResistance - 1.0f) * Time.deltaTime;
+        }
+        else if (m_rigidbody.velocity.y < -0.01f)
+        {
+            m_rigidbody.velocity += (Vector3.up * Physics.gravity.y) * (m_fallSpeed - 1.0f) * Time.deltaTime;
+        }
+
+        m_animator.SetFloat("yVelocity", m_rigidbody.velocity.y);
+    }
+}
