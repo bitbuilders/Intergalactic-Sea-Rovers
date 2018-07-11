ef88e2a85fb9fda35123045507664a3e4468d46a
Colin Misbach <colin@misbach.com>
2018-07-11 17:11:06 -0600

    Objects can now pulsate | Lanterns rotate to look nicer when swinging

---
+++ Assets/Scripts/Environment/InteractionBlink.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionBlink : MonoBehaviour
+{
+    [SerializeField] [Range(0.0f, 10.0f)] float m_blinkSpeed = 1.5f;
+    [SerializeField] Color m_baseColor = Color.green;
+
+    Renderer m_renderer;
+    Material m_material;
+    
+    void Start()
+    {
+        m_renderer = GetComponentInChildren<Renderer>();
+        m_material = m_renderer.material;
+    }
+    
+    void Update()
+    {
+        float emission = Mathf.PingPong(Time.time * m_blinkSpeed, 1.3f) + 0.0f;
+        Color finalColor = m_baseColor * Mathf.LinearToGammaSpace(emission);
+        m_material.SetColor("_EmissionColor", finalColor);
+    }
+}
--- Assets/Scripts/Environment/Lantern.cs
+++ Assets/Scripts/Environment/Lantern.cs
@@ -7,23 +7,37 @@ public class Lantern : MonoBehaviour
     [SerializeField] [Range(0.0f, 60.0f)] float m_forceRate = 30.0f;
     [SerializeField] [Range(0.0f, 500.0f)] float m_forceMagnitude = 100.0f;
     [SerializeField] Rigidbody m_lanternBody = null;
+    [SerializeField] GameObject m_lanternAnchor = null;
+    [SerializeField] GameObject m_lanternMesh = null;
+    [SerializeField] bool m_pushOnce = false;
 
     float m_forceTime = 0.0f;
 
     private void Start()
     {
         m_forceTime = m_forceRate;
+        if (m_pushOnce)
+            Shake();
     }
 
     private void Update()
     {
-        m_forceTime += Time.deltaTime;
-
-        if (m_forceTime >= m_forceRate)
+        if (!m_pushOnce)
         {
-            m_forceTime = 0.0f;
-            Shake();
+            m_forceTime += Time.deltaTime;
+
+            if (m_forceTime >= m_forceRate)
+            {
+                m_forceTime = 0.0f;
+                Shake();
+            }
         }
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 dir = m_lanternAnchor.transform.position - m_lanternBody.transform.position;
+        m_lanternMesh.transform.up = dir;
     }
 
     public void Shake()
